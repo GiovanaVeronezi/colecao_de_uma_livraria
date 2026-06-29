f3a22934ad7d7d7aa89aae7ba9ad4f27fef13268
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-06-29 19:46:14 -0300

    feat:  Retirando as rotas do app.js

--- src/app.js
+++ src/app.js
@@ -18,34 +18,4 @@ const app = express();
 routes(app);
 
 
-
-app.get("/livros/:id", async (req,res)=>{
-  const livroEncontrado = await livro.findById(req.params.id);
-  res.status(200).json(livroEncontrado);
-});
-
-app.put("/livros/:id", async(req,res)=>{
-
- const livroAtualizado = await livro.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {new:true}
- );
-
- res.status(200).json(livroAtualizado);
-
-});
-
-app.put("/livros/:id", async(req,res)=>{
-
- const livroAtualizado = await livro.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    {new:true}
- );
-
- res.status(200).json(livroAtualizado);
-
-});
-
 export default app;
