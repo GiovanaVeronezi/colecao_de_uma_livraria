e8272684752d23b8eca9948ecac67b707e658ee5
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-06-30 20:15:20 -0300

    feat: "Criando a lógica do método Delete"

--- src/controllers/livroControllers.js
+++ src/controllers/livroControllers.js
@@ -16,7 +16,7 @@ class LivroController {
     static async listarLivrosPorId (req, res){ 
         try{
             const id = req.params.id;
-        const livroEncontrado = await Livro.findById(id); //chama o model livro atraves do find 
+        const livroEncontrado = await Livro.findById(id);  
         res.status(200).json(livroEncontrado);
         } catch(erro){
             res.status(500).json({message: `${erro.message} - falha na requisição do livro`});
@@ -39,10 +39,22 @@ class LivroController {
      static async atualizarLivro (req, res){ 
         try{
             const id = req.params.id;
-        await Livro.findByIdAndUpdate(id, req.body); //chama o model livro atraves do find 
+        await Livro.findByIdAndUpdate(id, req.body); 
         res.status(200).json({message: "Livro atualizado"});
         } catch(erro){
             res.status(500).json({message: `${erro.message} - falha na atualização do livro`});
+        }
+        
+        
+    };
+
+    static async excluirLivro (req, res){ 
+        try{
+            const id = req.params.id;
+            await Livro.findByIdAndDelete(id); // removi declaração variavel
+            res.status(200).json({message: "Livro excluido com sucesso"});
+        } catch(erro){
+            res.status(500).json({message: `${erro.message} - falha na exclusão`});
         }
         
         
