c22f3435fe637b53ebaf03cb09b04562c4846446
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-07-08 19:39:36 -0300

    feat:"Ativando as rotas autores"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
-import express from "express";
+import express from "express";  
 import livros from "./livrosRoutes.js";
+import autores from "./autorRoutes.js"
 
-const routes = (app) => {
-  app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"));
-
-  app.use(express.json(), livros); //registra um middleware do Express que interpreta requisições com corpo JSON e disponibiliza os dados em req.body
+const routes = (app) => { // permite configurar o express
+  app.route("/").get((req, res) => res.status(200).send("Curso de Node.js")); 
+  app.use(express.json(), livros, autores);
+  //registra um middleware que interpreta requisições com corpo JSON e disponibiliza os dados em req.body e livros/autores ativam todas as rotas livros/autores
 };
 
 export default routes;
