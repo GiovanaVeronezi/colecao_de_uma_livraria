0df2f82b14f3c903ebfffe080226bbf0005a3cb4
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-07-12 18:24:57 -0300

    feat: Criando configurações da página principal

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <main className="home">
+
+      <section className="hero">
+
+        <h2>Bem-vinda ao Sistema da Biblioteca</h2>
+
+        <p>
+          Gerencie livros e autores de forma rápida e organizada.
+        </p>
+
+      </section>
+
+    </main>
+  );
+}
+
+export default Home;
