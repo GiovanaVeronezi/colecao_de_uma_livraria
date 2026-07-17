925c4781c0b635f29b931ae5440b500d87491628
GiovanaVeronezi <giovanamveronezi@outlook.com>
2026-07-16 23:15:22 -0300

    feat: Adicionando um comentário e removendo import desnecessário

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,11 +1,10 @@
 import express from "express";  
 import livros from "./livrosRoutes.js";
-import autores from "./autorRoutes.js"
 
 const routes = (app) => { // permite configurar o express
   app.route("/").get((req, res) => res.status(200).send("Curso de Node.js")); 
-  app.use(express.json(), livros, autores);
-  //registra um middleware que interpreta requisições com corpo JSON e disponibiliza os dados em req.body e livros/autores ativam todas as rotas livros/autores
+  app.use(express.json(), livros);
+  //registra um middleware que interpreta requisições com corpo JSON e disponibiliza os dados em req.body e livros ativam todas as rotas livros
 };
 
 export default routes;
